a020e021e7d13822b360b6604eaca05d2d30a7e6
Shikhar Mani Tripathi <tripathi.shekhar1970@outlook.com>
2026-01-14 23:51:59 +0530

    Linter PR modification
    major update in linter protocol, dependency addition, domain based PR request validation, with PR failure notification

---
+++ sim/verilator/deps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Common RTL dependencies (packages, interfaces)
+RTL_DEPS=(
+  rtl/common/pkg.sv
+  rtl/common/defines.sv
+)
+
+# Common TB dependencies
+TB_DEPS=(
+  tb/common/pkg.sv
+  tb/common/defines.sv
+)
