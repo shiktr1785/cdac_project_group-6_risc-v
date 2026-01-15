cf60de0d425451aa93290d061b846f381466ef6f
Shikhar Mani Tripathi <tripathi.shekhar1970@outlook.com>
2026-01-15 16:20:58 +0530

    ci: dry run -v2: RTL Lint failure 3

--- rtl/alu/bad_alu.sv
+++ rtl/alu/bad_alu.sv
@@ -1,7 +1,9 @@
 module bad_alu;
     logic a;
     logic c;
+    logic f;
 
     assign b = a;   // Error: 'b' is not declared
     assign c = d;   // Error: 'd' is not declared
+    assign g = f;   // Error: 'g' is not declared
 endmodule
