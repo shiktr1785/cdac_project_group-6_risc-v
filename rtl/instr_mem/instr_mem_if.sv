af7b8b000a5aac002d5b95a25f9402977058fe72
PrithwinPrakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-20 16:08:08 +0530

    [VERIFICATION][INSTR_MEM] Create reusable instruction memory interface #22
    Modified  parameter BUS to BUS_WIDTH

--- rtl/instr_mem/instr_mem_if.sv
+++ rtl/instr_mem/instr_mem_if.sv
@@ -2,11 +2,11 @@
 `define __INSTR_MEM_IF_SV
 
 interface instr_mem_if #(
-    integer BUS = 31 )(
+    integer BUS_WIDTH = 32 )(
     input logic clk);
 
-    logic          next_instr;                //next_instr is output when current instruction is successfully executed
-    logic [BUS:0] instruction;            //the current instruction
+    logic                next_instr;             //next_instr is output when current instruction is successfully executed
+    logic [BUS_WIDTH-1:0] instruction;            //the current instruction
 
     clocking drv_cb @(posedge clk);
         default input #1step output #1;
