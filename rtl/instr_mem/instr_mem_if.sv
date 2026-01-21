ac42fde3e0d4057eac077e3328db1e310ca9f951
PrithwinPrakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-21 15:38:41 +0530

    [VERIFICATION][INSTR_MEM] Create reusable instruction memory interface #22
    Modified directions for modport
    changed clk from logic to bit
    cleaned the code a bit

--- rtl/instr_mem/instr_mem_if.sv
+++ rtl/instr_mem/instr_mem_if.sv
@@ -3,29 +3,44 @@
 
 interface instr_mem_if #(
     integer BUS_WIDTH = 32 )(
-    input logic clk);
+    input bit clk);
 
-    logic                next_instr;             //next_instr is output when current instruction is successfully executed
-    logic [BUS_WIDTH-1:0] instruction;            //the current instruction
+    logic                   next_instr;   //next_instr is output when current instruction is successfully executed
+    logic [BUS_WIDTH-1:0]   instruction;  //the current instruction
 
+    `ifndef VERILATOR
     clocking drv_cb @(posedge clk);
-        default input #1step output #1;
+        
         //samples input before clock edge and output 1ns after clock edge
-        input next_instr;
-        //driver drives next_instr and reads the current instruction
+        default input #1step output #1;
         output instruction;
+        input next_instr; 
+           
     endclocking
 
     clocking mon_cb @(posedge clk);
-        default input #1step output #1;
-            input next_instr;              //Monitor reads both next_instr and instruction
-            output instruction;
-    endclocking
 
-    modport driver (clocking drv_cb); 
+        default input #1step output #1;
+        input instruction;
+        input next_instr;              
+        
+    endclocking
+    `endif
+
     //driver direction
-    modport monitor(clocking mon_cb);
+    modport driver 
+    (
+        output instruction,
+        input next_instr
+    ); 
+
     //monitor direction
+    modport monitor
+    (
+        input instruction,
+        input next_instr
+    );
+    
 
 endinterface // instr_mem_if
 
