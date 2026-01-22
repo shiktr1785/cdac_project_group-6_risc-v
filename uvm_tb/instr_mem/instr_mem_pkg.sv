fc4fa81abb2a32b2cafd195c64db825b72316f88
PrithwinPrakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-22 21:02:02 +0530

    [VERIFICATION][INSTR_MEM] Define instruction memory sequencer dependencies #40
    Created instruction memory sequencer dependencies
    instr_mem_seq_item.sv
    instr_mem_sequence.sv
    instr_mem_pkg.sv
    
    Added two conditions , In of Bounds and Out of Bounds sequences
    Pending Review from Sidd or Ritik

---
+++ uvm_tb/instr_mem/instr_mem_pkg.sv
@@ -0,0 +1,14 @@
+`ifndef INSTR_MEM_PKG_SV
+`define INSTR_MEM_PKG_SV
+
+package instr_mem_pkg;
+
+    import uvm_pkg::*;
+ 
+    `include "uvm_macros.svh"
+    `include "instr_mem_seq_item.sv"
+    `include "instr_mem_sequence.sv"
+    
+endpackage
+
+`endif
---
+++ uvm_tb/instr_mem/instr_mem_sequence.sv
@@ -0,0 +1,85 @@
+`ifndef __INSTR_MEM_SEQUENCE_SV
+`define __INSTR_MEM_SEQUENCE_SV
+
+class instr_mem_sequence extends uvm_sequence #(instr_mem_seq_item);
+
+    `uvm_object_utils(instr_mem_sequence)
+
+    // Memory Depth
+    localparam int MEM_DEPTH = 64;
+    protected int pc;
+    function new(string name = "instr_mem_sequence");
+        super.new(name);
+        pc=0;
+    endfunction: new
+
+    virtual task body();
+        instr_mem_seq_item req;
+        //Create tracker for internal address tracking
+        `uvm_info("SEQ","SEQ START",UVM_LOW);
+
+        //Repeat it MEM_DEPTH times
+        repeat(MEM_DEPTH) begin
+        req = instr_mem_seq_item::type_id::create("req");
+        start_item(req);
+        
+        req.current_pc = pc;
+
+        req.IOB_c.constraint_mode(1); //Enable IOB constraint
+        req.OOB_c.constraint_mode(0); //Disable OOB constraint
+
+        if(!req.randomize()) 
+            `uvm_error("SEQ","Randomization failed");
+   
+        finish_item(req);
+        `uvm_info("SEQ ITEM", $sformatf("Instruction at PC=%0d is %0h", pc, req.instruction), UVM_LOW);
+        pc=pc+1;
+        end
+        `uvm_info("SEQ", "SEQ END", UVM_LOW);
+    endtask: body
+
+endclass: instr_mem_sequence
+
+`endif
+
+//EXTENDED OUT OF BOUNDS SEQUENCE
+
+`ifndef __OOB_INSTR_MEM_SEQUENCE_SV
+`define __OOB_INSTR_MEM_SEQUENCE_SV
+
+class oob_instr_mem_sequence extends instr_mem_sequence;
+
+    `uvm_object_utils(oob_instr_mem_sequence)
+
+    function new(string name = "oob_instr_mem_sequence");
+        super.new(name);
+    endfunction: new
+
+    virtual task body();
+        instr_mem_seq_item req;
+        super.body();  // Call the base class body to complete in-bounds sequence first
+
+        `uvm_info("OOB_SEQ","OOB SEQ CROSSING BOUNDS",UVM_NONE)
+
+        //Repeat it 20 times
+        repeat(20) begin
+        req = instr_mem_seq_item::type_id::create("req");
+        start_item(req);
+        
+        req.IOB_c.constraint_mode(0);  // Turn off safety
+        req.OOB_c.constraint_mode(1); // Turn on danger
+        finish_item(req);
+        `uvm_info("OOB SEQ ITEM VIOLATED", $sformatf("Instruction at PC=%0d is %0h", pc, req.instruction), UVM_LOW);
+       
+        pc=pc+1;
+        end
+        `uvm_info("OOB SEQ","VIOLATION COMPLETE",UVM_LOW)
+    endtask
+
+endclass: oob_instr_mem_sequence
+
+`endif
+
+
+
+
