e90cccc07eb832ac77b4c13d27a8134561e6234b
“Alen <allen.sojan.koroth@gmail.com>
2026-01-23 11:12:03 +0530

    [VERIFICATION][ALU] Define ALU sequencer dependencies #38
    Had to make changes to the inputs and outputs and now modified the constraints for just illegal and legal opcodes

--- uvm_tb/alu/alu_sequence.sv
+++ uvm_tb/alu/alu_sequence.sv
@@ -25,16 +25,15 @@ class alu_sequence #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) extends uvm_seque
     // Constraints
 
     constraint opcode_c {
-        if (seq_item.inject_illegal_opcode) 
-            soft !(seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111});
-        else 
             soft seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111};
     }
 
-    constraint boundary_c {
+    constraint rs_data_boundary_c {
         soft seq_item.rs_data inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
-        if (seq_item.rs_data_sel) 
-            soft seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555}; // only taken as operand when rs_data_sel is 1
+    }
+
+    constraint imme_value_boundary_c {
+        soft seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
     }
 
 endclass
@@ -55,10 +54,6 @@ class alu_sequence_illegal_opcode #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) ex
     virtual task body();
         seq_item = alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH)::type_id::create("seq_item");
 
-        // Set the illegal opcode flag to 1
-        seq_item.inject_illegal_opcode = 1;
-        seq_item.rs_data_valid = 1; // Ensure rs_data is valid for this test
-
         // Start the sequence item
         start_item(seq_item);
         finish_item(seq_item);
@@ -67,14 +62,15 @@ class alu_sequence_illegal_opcode #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) ex
     // Constraints for illegal opcode
 
     constraint opcode_c {
-        if (seq_item.inject_illegal_opcode) 
             !(seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111});
     }
 
     constraint boundary_c {
         seq_item.rs_data inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
-        if (seq_item.rs_data_sel) 
-            seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555}; // only taken as operand when rs_data_sel is 1
+    }
+
+    constraint imme_value_boundary_c {
+        seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
     }
 
 endclass
@@ -95,10 +91,6 @@ class alu_sequence_legal_opcode #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) exte
     virtual task body();
         seq_item = alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH)::type_id::create("seq_item");
 
-        // Set the illegal opcode flag to 0 for legal opcode
-        seq_item.inject_illegal_opcode = 0;
-        seq_item.rs_data_valid = 1; // Ensure rs_data is valid for this test
-
         // Start the sequence item
         start_item(seq_item);
         finish_item(seq_item);
@@ -107,90 +99,15 @@ class alu_sequence_legal_opcode #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) exte
     // Constraints for legal opcode
 
     constraint opcode_c {
-        if (!seq_item.inject_illegal_opcode) 
             seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111};
     }
 
-    constraint boundary_c {
+    constraint rs_data_boundary_c {
         seq_item.rs_data inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
-        if (seq_item.rs_data_sel) 
-            seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555}; // only taken as operand when rs_data_sel is 1
     }
 
-endclass
-
-class alu_sequence_rs_data_invalid #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) extends alu_sequence #(BUS_WIDTH, OPCODE_WIDTH);
-
-    `uvm_object_param_utils(alu_sequence_rs_data_invalid #(BUS_WIDTH, OPCODE_WIDTH));
-    alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH) seq_item;
-    
-    // Constructor
-
-    function new(string name = "alu_sequence_rs_data_invalid");
-        super.new(name);
-    endfunction
-
-    // Body task
-
-    virtual task body();
-        seq_item = alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH)::type_id::create("seq_item");
-
-        // Set the rs_data_valid flag to 0 (invalid)
-        seq_item.rs_data_valid = 0;
-        seq_item.inject_illegal_opcode = 0; // Ensure opcode is legal for this test
-
-        // Start the sequence item
-        start_item(seq_item);
-        finish_item(seq_item);
-    endtask : body
-    
-    constraint opcode_c {
-        if (!seq_item.inject_illegal_opcode) 
-            seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111};
-    }
-
-    constraint boundary_c {
-        seq_item.rs_data inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
-        if (seq_item.rs_data_sel) 
-            seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555}; // only taken as operand when rs_data_sel is 1
-    }
-
-endclass
-
-class alu_sequence_rs_data_valid #(int BUS_WIDTH = 32, int OPCODE_WIDTH = 4) extends alu_sequence #(BUS_WIDTH, OPCODE_WIDTH);
-
-    `uvm_object_param_utils(alu_sequence_rs_data_valid #(BUS_WIDTH, OPCODE_WIDTH));
-    alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH) seq_item;
-    
-    // Constructor
-
-    function new(string name = "alu_sequence_rs_data_valid");
-        super.new(name);
-    endfunction
-
-    // Body task
-
-    virtual task body();
-        seq_item = alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH)::type_id::create("seq_item");
-
-        // Set the rs_data_valid flag to 1 (valid)
-        seq_item.rs_data_valid = 1;
-        seq_item.inject_illegal_opcode = 0; // Ensure opcode is legal for this test
-
-        // Start the sequence item
-        start_item(seq_item);
-        finish_item(seq_item);
-    endtask : body
-    
-    constraint opcode_c {
-        if (!seq_item.inject_illegal_opcode) 
-            seq_item.opcode inside { 4'b0000, 4'b1000, 4'b0001, 4'b0010, 4'b0011, 4'b0100, 4'b0101, 4'b1101, 4'b0110 ,4'b0111};
-    }
-
-    constraint boundary_c {
-        seq_item.rs_data inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
-        if (seq_item.rs_data_sel) 
-            seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555}; // only taken as operand when rs_data_sel is 1
+    constraint imme_value_boundary_c {
+        seq_item.imme_value inside {32'h00000000,  32'h00000001,  32'hFFFFFFFF,  32'h7FFFFFFF,  32'h80000000, 32'hAAAAAAAA, 32'h55555555};
     }
 
 endclass
--- uvm_tb/alu/alu_sequence_item.sv
+++ uvm_tb/alu/alu_sequence_item.sv
@@ -9,19 +9,14 @@ class alu_sequence_item #(
     `uvm_object_utils(alu_sequence_item #(BUS_WIDTH, OPCODE_WIDTH))
         
     // Variables
+        // Inputs
     rand logic [BUS_WIDTH - 1 : 0] rs_data;
-    rand logic [BUS_WIDTH - 1 : 0] imme_value;
-
+    rand logic [BUS_WIDTH - 1 : 0] imme_rs;
     randc logic [OPCODE_WIDTH - 1 : 0] opcode;
-    
-         bit rs_data_valid;
-         bit rs_data_sel;
          
         // Outputs
-    
     logic [BUS_WIDTH - 1 : 0] alu_data_out;
-    logic alu_data_valid;
-    logic op_done; 
+    logic alu_data_valid; 
 
     // Constructor
 
@@ -31,8 +26,8 @@ class alu_sequence_item #(
 
     // Print method 
     function void print();
-        `uvm_info(get_type_name(), $sformatf("rs_data: %0h, imme_value: %0h, opcode: %0h, rs_data_sel: %0b, rs_data_valid: %0b, alu_data_out: %0h, alu_data_valid: %0b, op_done: %0b",
-            rs_data, imme_value, opcode, rs_data_sel, rs_data_valid, alu_data_out, alu_data_valid, op_done), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("rs_data: %0h, imme_rs: %0h, opcode: %0h, alu_data_out: %0h, alu_data_valid: %0b",
+            rs_data, imme_rs, opcode, alu_data_out, alu_data_valid), UVM_LOW)
     endfunction
 
 endclass : alu_sequence_item
