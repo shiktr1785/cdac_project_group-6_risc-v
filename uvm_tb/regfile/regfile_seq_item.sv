cdfe3d46591442b4536bab239c2b3bdfb16a0074
“Alen <allen.sojan.koroth@gmail.com>
2026-01-24 14:50:17 +0530

    [VERIFICATION][REGFILE] Define regfile sequencer dependencies #37
    Corrected the input and output signals so that rand is only given to inputs

--- uvm_tb/regfile/regfile_seq_item.sv
+++ uvm_tb/regfile/regfile_seq_item.sv
@@ -11,19 +11,23 @@ class regfile_seq_item #(
 
         //INPUTS
 
-    rand logic alu_data_valid;
-    rand logic [BUS_WIDTH-1:0] rs_data_mux;
     rand logic rs_store;
-    rand logic [BUS_WIDTH-1:0] imme_data;
     rand logic rs_addr_valid;
+
+    rand logic [BUS_WIDTH-1:0] imme_data;
+    
     rand logic rd_wr_en;
     rand logic [ADDR_WIDTH-1:0] rs1_rs2_rd;
 
+    rand logic alu_data_valid;
+    rand logic [BUS_WIDTH-1:0] alu_data_out;
+    
+
         //OUTPUTS
 
-    logic alu_data_valid;
     logic op_done;
     logic [BUS_WIDTH-1:0] rs_data_mux;
+    logic [BUS_WIDTH-1:0] rs_data;
 
     // Constructor
 
@@ -34,8 +38,8 @@ class regfile_seq_item #(
     // Print method 
 
     function void print();
-        `uvm_info(get_type_name(), $sformatf("alu_data_valid: %0b, rs_data_mux: %0h, rs_store: %0b, imme_data: %0h, rs_addr_valid: %0b, rd_wr_en: %0b, rs1_rs2_rd: %0h, op_done: %0b",
-            alu_data_valid, rs_data_mux, rs_store, imme_data, rs_addr_valid, rd_wr_en, rs1_rs2_rd, op_done), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("rs_store: %0b, rs_addr_valid: %0b, imme_data: %0h, rd_wr_en: %0b, rs1_rs2_rd: %0h, alu_data_valid: %0b, alu_data_out: %0h, op_done: %0b, rs_data_mux: %0h, rs_data: %0h",
+            rs_store, rs_addr_valid, imme_data, rd_wr_en, rs1_rs2_rd, alu_data_valid, alu_data_out, op_done, rs_data_mux, rs_data), UVM_LOW)
     endfunction
     
     
