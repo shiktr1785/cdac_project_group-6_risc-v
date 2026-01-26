dd47acee5a27c7ef62396b7a856555ac7b068a99
“Alen <allen.sojan.koroth@gmail.com>
2026-01-26 12:30:28 +0530

    [VERIFICATION][REGFILE] Define regfile sequencer dependencies #37
    made some changes for randomization by avoiding the control signals and added uvm info high message in each function and task for debugging

--- uvm_tb/regfile/regfile_seq_item.sv
+++ uvm_tb/regfile/regfile_seq_item.sv
@@ -4,22 +4,22 @@
 class regfile_seq_item #(
     int ADDR_WIDTH = 15,
     int BUS_WIDTH = 32
-)extends uvm_sequence;
+)extends uvm_sequence_item;
     `uvm_object_utils(regfile_seq_item);
 
     // Data members
 
         //INPUTS
 
-    rand logic rs_store;
-    rand logic rs_addr_valid;
+         logic rs_store;
+         logic rs_addr_valid;
 
     rand logic [BUS_WIDTH-1:0] imme_data;
     
-    rand logic rd_wr_en;
+         logic rd_wr_en;
     rand logic [ADDR_WIDTH-1:0] rs1_rs2_rd;
 
-    rand logic alu_data_valid;
+         logic alu_data_valid;
     rand logic [BUS_WIDTH-1:0] alu_data_out;
     
 
@@ -33,13 +33,18 @@ class regfile_seq_item #(
 
     function new(string name = "regfile_seq_item");
         super.new(name);
+        `uvm_info(get_name(), "Constructor", UVM_HIGH)
+        
     endfunction
 
-    // Print method 
+    // Display method 
 
-    function void print();
+    function void display();
         `uvm_info(get_type_name(), $sformatf("rs_store: %0b, rs_addr_valid: %0b, imme_data: %0h, rd_wr_en: %0b, rs1_rs2_rd: %0h, alu_data_valid: %0b, alu_data_out: %0h, op_done: %0b, rs_data_mux: %0h, rs_data: %0h",
             rs_store, rs_addr_valid, imme_data, rd_wr_en, rs1_rs2_rd, alu_data_valid, alu_data_out, op_done, rs_data_mux, rs_data), UVM_LOW)
+
+        `uvm_info(get_name(), "Display", UVM_HIGH)
+        
     endfunction
     
     
--- uvm_tb/regfile/regfile_sequence.sv
+++ uvm_tb/regfile/regfile_sequence.sv
@@ -1,9 +1,10 @@
 `ifndef __REGFILE_SEQUENCE_SV
 `define __REGFILE_SEQUENCE_SV
 
-//  Class: regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15)
+//  Class: regfile_sequence 
 //
-class regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15) extends uvm_object;
+class regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15) extends uvm_sequence;
+
     `uvm_object_param_utils(regfile_sequence #(BUS_WIDTH,ADDR_WIDTH));
     regfile_seq_item #(ADDR_WIDTH,BUS_WIDTH) seq_item;
 
@@ -11,6 +12,7 @@ class regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15) extends uvm_objec
 
     function new(string name = "regfile_sequence");
         super.new(name);
+        `uvm_info(get_name(), "Constructor", UVM_HIGH)
     endfunction: new
 
     // Body task
@@ -21,11 +23,14 @@ class regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15) extends uvm_objec
         // Start the sequence item
         start_item(seq_item);
         finish_item(seq_item);
+        `uvm_info(get_name(), "Body", UVM_HIGH)
+        
     endtask : body
 
     // Constraints
-    
-endclass: regfile_sequence #(int BUS_WIDTH=32 , int ADDR_WIDTH=15)
 
+
+    
+endclass: regfile_sequence 
 
 `endif
