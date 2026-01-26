130599768a62ad81e8e170991cc5aa36c7faa64b
Prithwin Prakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-26 20:06:48 +0530

    [VERIFICATION][INSTR_MEM] Develop instruction memory UVM verification environment #28
    agent file was cleaned and checked

--- uvm_tb/instr_mem/instr_mem_agent.sv
+++ uvm_tb/instr_mem/instr_mem_agent.sv
@@ -10,19 +10,17 @@ class instr_mem_agent #(parameter BUS_WIDTH = 32) extends uvm_agent;
 
     function new(string name="instr_mem_agent", uvm_component parent);
         super.new(name, parent);
-        `uvm_info(get_name(), "New", UVM_MEDIUM)
+        `uvm_info(get_name(),"New",UVM_MEDIUM)
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        
-        monitor = instr_mem_monitor#(BUS_WIDTH)::type_id::create("monitor", this);
-
+        monitor=instr_mem_monitor#(BUS_WIDTH)::type_id::create("monitor", this);
         if(get_is_active() == UVM_ACTIVE) begin
-            driver = instr_mem_driver#(BUS_WIDTH)::type_id::create("driver", this);
-            sequencer = instr_mem_sequencer#(BUS_WIDTH)::type_id::create("sequencer", this);
+            driver=instr_mem_driver#(BUS_WIDTH)::type_id::create("driver", this);
+            sequencer=instr_mem_sequencer#(BUS_WIDTH)::type_id::create("sequencer", this);
         end
-        `uvm_info(get_name(), "Build Phase", UVM_MEDIUM)
+        `uvm_info(get_name(),"Build Phase",UVM_MEDIUM)
     endfunction: build_phase
 
     function void connect_phase(uvm_phase phase);
@@ -30,7 +28,7 @@ class instr_mem_agent #(parameter BUS_WIDTH = 32) extends uvm_agent;
             driver.seq_item_port.connect(sequencer.seq_item_export);
         end
 
-        `uvm_info(get_name(), "Connect Phase", UVM_MEDIUM)
+        `uvm_info(get_name(),"Connect Phase",UVM_MEDIUM)
         
     endfunction: connect_phase
 endclass
