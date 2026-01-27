8fedaef092dcaf8010c2c4af465b73a84f0447d8
Prithwin Prakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-28 00:42:56 +0530

    Update instr_mem_seq_item.sv

--- uvm_tb/instr_mem/instr_mem_seq_item.sv
+++ uvm_tb/instr_mem/instr_mem_seq_item.sv
@@ -1,46 +1,54 @@
-`ifndef __INSTR_MEM_SEQ_ITEM_SV
-`define __INSTR_MEM_SEQ_ITEM_SV
+    `ifndef __INSTR_MEM_SEQ_ITEM_SV
+    `define __INSTR_MEM_SEQ_ITEM_SV
 
-class instr_mem_seq_item#(BUS_WIDTH = 32) extends uvm_sequence_item;
-    
-    //Factory Registration
-    `uvm_object_utils(instr_mem_seq_item#(BUS_WIDTH))
-    // Data members
-    // 1 - OP_done,push out the instruction; 0 - Stop instruction 
-    rand bit                    next_instr;
-         bit [BUS_WIDTH-1:0]    instruction;
-         bit                    instr_valid;
+    class instr_mem_seq_item#(
+        parameter ADDR=6,
+        parameter BUS_WIDTH=32
+    ) extends uvm_sequence_item;
+        
+        //Factory Registration
+        `uvm_object_utils(instr_mem_seq_item#(ADDR, BUS_WIDTH))
+        local int DEPTH=64;
+        // Data members
+        // 1 - next_instr,push out the instruction; 0 - Stop instruction 
+        rand bit                    next_instr;
+        
+        rand bit                    store_en;
+        rand bit [ADDR-1:0]         store_address; 
+        rand bit [BUS_WIDTH-1:0]    store_data;
 
-    //To track current PC
-     int current_pc;
+        // Output members
+            bit [BUS_WIDTH-1:0]    instruction;
+            bit                    instr_valid;
 
-     //Constraints
+        //To track current PC
+        int current_pc;
 
-     //Constraint IOB(IN of Bounds) respect the boundary within MEM_DEPTH
-    constraint IOB_c { 
-        (current_pc<64)-> next_instr==1;
-        (current_pc>=64)-> next_instr==0;
-    }  
+        //Constraints
 
-    //Constraint OOB(Out of Bounds) to cross the boundary of MEM_DEPTH
-    constraint OOB_c { 
-    (current_pc>=64)-> next_instr==1;
-    }
+        //Constraint IOB(IN of Bounds) respect the boundary within MEM_DEPTH
+        constraint IOB_c { 
+            (current_pc < DEPTH)-> next_instr==1;
+            (current_pc >= DEPTH)-> next_instr==0;
+        }  
 
-     //Constructor
-    function new(string name = "instr_mem_seq_item");
-        super.new(name);
-        IOB_c.constraint_mode(1); //Enable IOB constraint by default
-        OOB_c.constraint_mode(0); //Disable OOB constraint by default
-    endfunction
-    
-    function void display();
-        //Prints the status of the next_instr
-        `uvm_info("next_instr_status", $sformatf("next_instr = %0b", next_instr), UVM_LOW)  
-    endfunction
+        //Constraint OOB(Out of Bounds) to cross the boundary of MEM_DEPTH
+        constraint OOB_c { 
+            (current_pc >= DEPTH)-> next_instr==1;
+        }
 
-endclass:instr_mem_seq_item
-    
-`endif
+        //Constructor
+        function new(string name = "instr_mem_seq_item");
+            super.new(name);
+            IOB_c.constraint_mode(1); //Enable IOB constraint by default
+            OOB_c.constraint_mode(0); //Disable OOB constraint by default
+        endfunction
+        
+        function void display();
+            //Prints the status of the item
+            `uvm_info("seq_item_status", $sformatf("next_instr = %0b, store_en = %0b, addr = %0h, data = %0h", next_instr, store_en, store_address, store_data), UVM_LOW)  
+        endfunction
 
-
+    endclass:instr_mem_seq_item
+        
+    `endif
