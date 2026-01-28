b719aa6b8416a1d06c7b4b8f0f196162c1551185
Prithwin Prakash <133040306+PrithwinPrakash@users.noreply.github.com>
2026-01-29 02:04:42 +0530

    24 verificationdecoder create reusable instruction decoder interface #52
    24
    updated all interfaces

--- rtl/alu/alu_if.sv
+++ rtl/alu/alu_if.sv
@@ -1,57 +1,52 @@
 `ifndef __ALU_IF_SV
 `define __ALU_IF_SV
 
-interface alu_if#(
-    integer BUS_WIDTH = 32, 
-    integer OPCODE_WIDTH = 4)
-    (input bit clk);
+interface alu_if #(
+    parameter BUS_WIDTH = 32, 
+    parameter OPCODE_WIDTH = 4
+) (input bit clk);
 
+    // Signals
     logic [BUS_WIDTH-1:0]       imme_rs;        // Immediate operations
     logic [BUS_WIDTH-1:0]       rs_data;        // RS Data
     logic [OPCODE_WIDTH-1:0]    opcode;         // Op_code
     logic [BUS_WIDTH-1:0]       alu_data_out;   // Output from ALU
     logic                       alu_data_valid; // Check whether output from alu valid or not
 
-     
     `ifndef VERILATOR
-    
+    // Clocking block for synchronization
     clocking drv_cb @(posedge clk);
-
-        default input #1step output #1;      
+        default input #1step output #1;
         output imme_rs;
         output opcode;
-        
-        input rs_data;
-        input alu_data_out;
-        input alu_data_valid;
-
+        input  rs_data;
+        input  alu_data_out;
+        input  alu_data_valid;
     endclocking
 
     clocking mon_cb @(posedge clk);
-        
         default input #1step output #1;
-        input rs_data;
-        input alu_data_out;
-        input alu_data_valid;
-        input imme_rs;
-        input opcode;
-
+        input  rs_data;
+        input  alu_data_out;
+        input  alu_data_valid;
+        input  imme_rs;
+        input  opcode;
     endclocking
     `endif
 
-    //driver direction
-    modport driver(
+    // Modport for driver
+    modport driver (
+        input  clk,
         output imme_rs,
         output opcode,
-        
-        input rs_data,
-        input alu_data_out,
-        input alu_data_valid
-
+        input  rs_data,
+        input  alu_data_out,
+        input  alu_data_valid
     ); 
 
-    //monitor direction
-    modport monitor(
+    // Modport for monitor
+    modport monitor (
+        input clk,
         input rs_data,
         input alu_data_out,
         input alu_data_valid,
@@ -59,6 +54,7 @@ interface alu_if#(
         input opcode
     );
 
-endinterface
+endinterface: alu_if
 
 `endif
+
