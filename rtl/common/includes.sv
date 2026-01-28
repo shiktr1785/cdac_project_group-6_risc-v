f084423f5876520af1e4eff649c628d3d9c0b45e
VMOnGit <sidharthmenonim@gmail.com>
2026-01-28 15:36:07 +0530

    [DESIGN][INTEGRATION] Create include file for top module in rtl/common
    updated includes and instruction initialization files

--- rtl/common/includes.sv
+++
@@ -1,7 +0,0 @@
-/* verilator lint_off MULTITOP*/
-`include "rtl/alu/alu.sv"
-`include "rtl/instr_mem/instr_mem_v1.sv"
-`include "rtl/regfile/regfile.sv"
-`include "rtl/decoder/decoder_v2.sv"
-/*verilator lint_on MULTITOP*/
-
